6135104bf7b2dd3df7d6e3edd76aa574667b5beb
yui <gakusyuu112@gmail.com>
2021-05-26 22:21:43 +0000

    add testdata

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([MessagesTableSeeder::class]);
+        $this->call([TasksTableSeeder::class]);
     }
 }
---
+++ database/seeds/TasksTbleSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TasksTbleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('tasks')->insert([
+            'status' => 'test status 1',
+            'content' => 'test content 1'
+        ]);
+        DB::table('tasks')->insert([
+            'status' => 'test status 2',
+            'content' => 'test content 2'
+        ]);
+        DB::table('tasks')->insert([
+            'status' => 'test status 3',
+            'content' => 'test content 3'
+        ]);
+    }
+}
