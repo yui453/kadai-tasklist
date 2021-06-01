49cd2aea6b76a355ca3c47c77f99a01307b1635c
yui <gakusyuu112@gmail.com>
2021-06-01 08:42:58 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,9 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::resource('tasks','TasksController');
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
